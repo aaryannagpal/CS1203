7516dfd0c00a03a9090a763627173190f1899e9b
Aaryan Nagpal <aaryannagpal65@gmail.com>
2022-11-06 22:27:30 +0530

    linked list created

---
+++ Linked_List/Linked_Link.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+
+struct node
+{
+    int data;
+    struct node *link; //only one link for the next node
+};
+
+int main(){
+    //creating the first node
+    struct node *head = NULL; //initializing
+    head = (struct node*)malloc(sizeof(struct node)); //typecasting the pointer
+
+    head->data = 100;
+    head->link = NULL;
+
+    struct node *new = malloc(sizeof(struct node)); //creating new pointer
+    for (int i = 0; i <10;++i){
+        new->data = i;
+        new->link = NULL;
+        head->link = new;
+        head = new;
+        printf("%d\n",new->data);
+    }
+    return 0;
+}
